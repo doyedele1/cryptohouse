c39b6698b480c41154d06c5b624a725098392f6f
Demilade Oyedele <demilade.oyedele@gmail.com>
2021-11-21 09:52:00 +0100

    Cryptocurrencies page completed

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -8,10 +8,10 @@ import { Cryptocurrencies, News } from '../components';
 const { Title } = Typography; //destructuring the Typography.Title component
 
 const Homepage = () => {
-    const { data, isFetching } = useGetCryptosQuery();
-
+    const { data, isFetching } = useGetCryptosQuery(10);
     const globalStats = data?.data?.stats;
-    // console.log(globalStats);
+    
+    console.log(globalStats);
 
     if (isFetching) return "Loading data....";
 
@@ -19,21 +19,11 @@ const Homepage = () => {
         <>
             <Title level={2} className="heading">Global Crypto Statistics</Title>
             <Row>
-                <Col span={12}>
-                    <Statistic title="Total Cryptocurrencies" value={ globalStats.total } />
-                </Col>
-                <Col span={12}>
-                    <Statistic title="Total Exchanges" value={ millify(globalStats.totalExchanges) } />
-                </Col>
-                <Col span={12}>
-                    <Statistic title="Total Market Cap" value={ millify(globalStats.totalMarketCap) } />
-                </Col>
-                <Col span={12}>
-                    <Statistic title="Total 24-hour Volume" value={ millify(globalStats.total24hVolume) } />
-                </Col>
-                <Col span={12}>
-                    <Statistic title="Total Markets" value={ millify(globalStats.totalMarkets) } />
-                </Col>
+                <Col span={12}><Statistic title="Total Cryptocurrencies" value={ globalStats.total } /></Col>
+                <Col span={12}><Statistic title="Total Exchanges" value={ millify(globalStats.totalExchanges) } /></Col>
+                <Col span={12}><Statistic title="Total Market Cap" value={ millify(globalStats.totalMarketCap) } /></Col>
+                <Col span={12}><Statistic title="Total 24-hour Volume" value={ millify(globalStats.total24hVolume) } /></Col>
+                <Col span={12}><Statistic title="Total Markets" value={ millify(globalStats.totalMarkets) } /></Col>
             </Row>
 
             <div className="home-heading-container">
