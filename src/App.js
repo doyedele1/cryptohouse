bdc155a24630e4b28fc0d19f0d0d31943b31d6b2
doyedele1 <demilade.oyedele@gmail.com>
2022-04-20 16:23:54 +0100

    fixed-bugs-related-to-API

--- src/App.js
+++ src/App.js
@@ -20,11 +20,11 @@ const App = () => (
                 <Layout>
                     <div className="routes">
                         <Routes>
-                            <Route exact path="/" component={<Homepage/>} />
-                            <Route exact path="/exchanges" component={<Exchanges/>} />
-                            <Route exact path="/cryptocurrencies" component={<Cryptocurrencies/>} />
-                            <Route exact path="/crypto/:coinId" component={<CryptoDetails/>} />
-                            <Route exact path="/news" component={<News/>} />
+                            <Route exact path="/" element={<Homepage/>} />
+                            <Route exact path="/exchanges" element={<Exchanges/>} />
+                            <Route exact path="/cryptocurrencies" element={<Cryptocurrencies/>} />
+                            <Route exact path="/crypto/:coinId" element={<CryptoDetails/>} />
+                            <Route exact path="/news" element={<News/>} />
                         </Routes>
                     </div>
                 </Layout>
