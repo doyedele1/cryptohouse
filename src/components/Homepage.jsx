f8c11a7dee7158f72c8b7b0d9f30d46a7c483f66
doyedele1 <demilade.oyedele@gmail.com>
2022-04-20 15:20:28 +0100

    refactored-Homepage-component

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -18,11 +18,36 @@ const Homepage = () => {
         <>
             <Title level={2} className="heading">Global Crypto Statistics</Title>
             <Row gutter={[32, 32]}>
-                <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
-                <Col span={12}><Statistic title="Total Exchanges" value={ millify(globalStats.totalExchanges) } /></Col>
-                <Col span={12}><Statistic title="Total Market Cap" value={ millify(globalStats.totalMarketCap) } /></Col>
-                <Col span={12}><Statistic title="Total 24-hour Volume" value={ millify(globalStats.total24hVolume) } /></Col>
-                <Col span={12}><Statistic title="Total Markets" value={ millify(globalStats.totalMarkets) } /></Col>
+                <Col span={12}>
+                    <Statistic
+                        title="Total Cryptocurrencies"
+                        value={globalStats.total}
+                    />
+                </Col>
+                <Col span={12}>
+                    <Statistic
+                        title="Total Exchanges"
+                        value={millify(globalStats.totalExchanges)}
+                    />
+                </Col>
+                <Col span={12}>
+                    <Statistic
+                        title="Total Market Cap:"
+                        value={`$${millify(globalStats.totalMarketCap)}`}
+                    />
+                </Col>
+                <Col span={12}>
+                    <Statistic
+                        title="Total 24-hour Volume"
+                        value={`$${millify(globalStats.total24hVolume)}`}
+                    />
+                </Col>
+                <Col span={12}>
+                    <Statistic
+                        title="Total Markets"
+                        value={millify(globalStats.totalMarkets)}
+                    />
+                </Col>
             </Row>
 
             <div className="home-heading-container">
