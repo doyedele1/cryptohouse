c349fcc376a5a1466889dc34d7b46688f1108240
doyedele1 <demilade.oyedele@gmail.com>
2022-04-20 17:31:06 +0100

    completed-the-mobile-responsiveness-logic

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Line } from 'react-chartjs-2';
 import { Col, Row, Typography } from 'antd';
 
@@ -9,8 +9,11 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
     const coinTimestamp = [];
 
     for (let i = 0; i < coinHistory?.data?.history?.length; i++) {
-        coinPrice.push(coinHistory.data.history[i].price);
-        coinTimestamp.push( new Date(coinHistory.data.history[i].timestamp).toLocaleDateString());
+        coinPrice.push(coinHistory?.data?.history[i].price);
+    }
+
+    for (let i = 0; i < coinHistory?.data?.history?.length; i++) {
+        coinTimestamp.push(new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString());
     }
 
     const data = {
@@ -43,7 +46,7 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
             <Row className="chart-header">
                 <Title level={2} className="chart-title">{coinName} Price Chart</Title>
                 <Col className="price-container">
-                    <Title level={5} className="price-change">{coinHistory?.data?.change}%</Title>
+                    <Title level={5} className="price-change">Change: {coinHistory?.data?.change}%</Title>
                     <Title level={5} className="current-price">Current {coinName} Price: $ {currentPrice}</Title>
                 </Col>
             </Row>
