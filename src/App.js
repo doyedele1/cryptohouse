81cdc89b1263e068649332159d546ebe329522cf
doyedele1 <demilade.oyedele@gmail.com>
2023-10-18 00:39:24 +0100

    testing deployment

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './App.css';
+import { 
+    Nav,
+    Exchanges,
+    Homepage,
+    Cryptocurrencies,
+    News,
+    CryptoDetails 
+} from './components';
+import { Routes, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+const App = () => (
+        <div className="app">
+            <div className="nav">
+                <Nav />
+            </div>
+            <div className="main">
+                <Layout>
+                    <div className="routes">
+                        <Routes>
+                            <Route exact path="/" element={<Homepage/>} />
+                            <Route exact path="/exchanges" element={<Exchanges/>} />
+                            <Route exact path="/cryptocurrencies" element={<Cryptocurrencies/>} />
+                            <Route exact path="/crypto/:coinId" element={<CryptoDetails/>} />
+                            <Route exact path="/news" element={<News/>} />
+                        </Routes>
+                    </div>
+                </Layout>
+                <div className="footer">
+                    <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+                        Demilade Oyedele © 2022<br />
+                        All rights reserved.
+                    </Typography.Title>
+                    <Space>
+                        <Link to="/">Home</Link>
+                        <Link to="/exchanges">Exchanges</Link>
+                        <Link to="/news">News</Link>
+                    </Space>
+                </div>
+            </div>
+        </div>
+);
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import App from './App';
+import 'antd/dist/antd.css';
+import store from './app/store';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Router>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
