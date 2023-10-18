68d8832d771ede9b900f03f98345f8eb9661bf5d
doyedele1 <demilade.oyedele@gmail.com>
2023-10-18 01:02:41 +0100

    Updates

---
+++ precache-manifest.f6ba9ff8cd3a242b9e5c7b9266606e88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e435ee84e1cb24cf9ace6d2de8086cab",
+    "url": "/cryptohouse/index.html"
+  },
+  {
+    "revision": "113089b6c3b7d47d4558",
+    "url": "/cryptohouse/static/css/2.cc91c9c6.chunk.css"
+  },
+  {
+    "revision": "4fa3a5dd20e55b929c09",
+    "url": "/cryptohouse/static/css/main.bd284f70.chunk.css"
+  },
+  {
+    "revision": "113089b6c3b7d47d4558",
+    "url": "/cryptohouse/static/js/2.188516f8.chunk.js"
+  },
+  {
+    "revision": "618ee64dde60057540e84b8e27b52dc2",
+    "url": "/cryptohouse/static/js/2.188516f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fa3a5dd20e55b929c09",
+    "url": "/cryptohouse/static/js/main.721ca5b0.chunk.js"
+  },
+  {
+    "revision": "10b4e00ead59e09f7bad",
+    "url": "/cryptohouse/static/js/runtime-main.d49c25c0.js"
+  }
+]);
